e8483c169601015815743644c7fa23ca857890cb
Syeda-Rohab <syedarohab8@gmail.com>
2024-07-18 09:16:26 -0700

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,23 @@
+// topic ;
+
+// variables in typescript ;
+
+// we make variables in 3 types ;
+
+// 1 ) let ;
+
+// 2 ) var ;
+
+// 3 ) const ;
+
+// what are variables ?
+
+// variables are like containers that store values and datatypes ;
+
+// we will not use var because it dont detect errors ;
+
+//  let ;
+
+// const = we will use const when something is unchangeable ;
+
+// comments = when we dont want to run execute these lines ;
